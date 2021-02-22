9dbef671b48b58a0e611c7db045e2546111453ac
Volodymyr Mashchenko <volodymyr.mashchenko@agiliway.com>
2021-02-22 12:13:55 +0200

    fix DB HW

--- src/sql_basics/steps_7.sql
+++ src/sql_basics/steps_7.sql
@@ -1,7 +1,7 @@
-    
+
 -- Select all active posts
 SELECT posts.*, users.first_name, users.last_name FROM posts
-	JOIN users on posts.autor = users.id 
+	JOIN users on posts.autor = users.id
 	WHERE posts.active = 1
     ORDER BY posts.createAt desc
     LIMIT 10
@@ -10,14 +10,14 @@ SELECT posts.*, users.first_name, users.last_name FROM posts
 SELECT * FROM blog.posts WHERE reviewed = 0;
 
 -- Count fo users posts
-SELECT COUNT(posts.id), users.first_name, users.last_name  FROM posts
-	JOIN users on posts.autor = users.id 
+SELECT COUNT(posts.id) as count,users.id, users.first_name, users.last_name, email  FROM posts
+	JOIN users on posts.autor = users.id
 	WHERE posts.active = 1
-    GROUP BY first_name, last_name;
+    GROUP BY id, first_name, last_name, email;
 
 -- Select all active posts of users
 SELECT posts.*, users.first_name, users.last_name FROM posts
-	JOIN users on posts.autor = users.id 
+	JOIN users on posts.autor = users.id
 	WHERE posts.active = 1 and posts.autor IN (SELECT id FROM users WHERE id IN (2, 3))
     ORDER BY posts.createAt desc
     ;
@@ -31,7 +31,7 @@ SELECT title, text, active, createAt, updateAt
 
 -- Select all active posts
 SELECT COUNT(posts.id) as count, users.first_name, users.last_name FROM posts
-	JOIN users on posts.autor = users.id 
+	JOIN users on posts.autor = users.id
 	WHERE posts.active = 1
 	GROUP BY first_name, last_name
     HAVING count > 2
